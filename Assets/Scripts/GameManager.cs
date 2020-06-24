0f6be5695345e90e137b43c0bd568734c965236a
MeeSh0o <ljhwuwu@qq.com>
2020-06-24 15:54:03 +0800

    一点点东西

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+
+    private void Awake()
     {
-        
+        if(instance != null)
+        {
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOver()
     {
-        
+
+    }
+
+    public void StartBattle()
+    {
+
     }
 }
+
+/*
+ * gamemanaer传递角色数据给battlemanager，由battlemanager自行开始比赛，battlemanager在比赛完成后，将结果报告给gameManager
+ *      需要定义卡组、单卡数据结构
+ * 
+ */
